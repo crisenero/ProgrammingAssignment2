2f7de46da4e6fa59f917ae4091245b6044bfb76d
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    Committing changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2: Lexical Scoping
+## Functions to cache/retrieve the inverse of a matrix
+## Demonstrate knowledge in lexical scoping of objects
 
-## Write a short comment describing this function
+## Getters and setters to enable caching
+## Assumption: 'x' was pre-validated to be an invertible matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        ## set 'i' to NULL whenever mackeCacheMatrix is instantiated
+        i <- NULL
+        
+        ## allow assignment of new matrix
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        
+        ## allow to read matrix from outside of makeCacheMatrix
+        get <- function() x
+        
+        ## cache passed inverse matrix
+        setInverse <- function(inverse) i <<- inverse
+        
+        ## return cached inverse matrix
+        getInverse <- function() i
+        list(set = set, get = get,
+               setInverse = setInverse,
+               getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+## Assumption:
+## 'x' is an instance of makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## get cached inverse matrix
+        i <- x$getInverse()
+        
+        ## if exists in cache, return cached data
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        
+        ## if data doesn't exist in cache perform the following
+        ## 1. get matrix
+        ## 2. solve the inverse matrix
+        ## 3. cache inverse matrix
+        ## 4. return inverse matrix
+        data <- x$get()
+        i <- solve(data, ...)
+        message("caching inverse matrix")
+        x$setInverse(i)
+        i
 }
